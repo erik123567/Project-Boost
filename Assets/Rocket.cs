5a4216128af172c801171ef810ec88b739ffbd02
erik123567 <erik123567@gmail.com>
2018-03-05 16:26:17 -0500

    added key prints

---
+++ Assets/Rocket.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("thrusting");
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("going right");
+            
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            print("rotaiotn left");
+        }
+    }
+}
